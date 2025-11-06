7b682ef399402430bddb607f1f0e2d00ab3ab9e3
Jahi Omari Ghansam <85849036+jahi-omari@users.noreply.github.com>
2025-11-05 20:33:57 -0500

    Added Navbar componenet code and inserted placeholder logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,11 @@
 import React from 'react'
+import Navbar from './components/navbar'
 
 const App = () => {
   return (
     <>
-      <nav className="bg-indigo-700 border-b border-indigo-500">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex h-20 items-center justify-between">
-          <div
-            className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
-          >
-            {/* <!-- Logo --> */}
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
-              <img
-                className="h-10 w-auto"
-                src="images/logo.png"
-                alt="React Jobs"
-              />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2"
-                >React Jobs</span
-              >
-            </a>
-            <div className="md:ml-auto">
-              <div className="flex space-x-2">
-                <a
-                  href="/index.html"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
-                >
-                <a
-                  href="/jobs.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
-                >
-                <a
-                  href="/add-job.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    {/* <!-- Navbar --> */}
+      <Navbar/>
 
     {/* <!-- Hero --> */}
     <section className="bg-indigo-700 py-20 mb-4">
@@ -53,10 +16,10 @@ const App = () => {
           <h1
             className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
           >
-            Become a React Dev
+            Plan Your Next Trip
           </h1>
           <p className="my-4 text-xl text-white">
-            Find the React job that fits your skills and needs
+            Keep track of all your past, current and upcoming trips!
           </p>
         </div>
       </div>
@@ -67,18 +30,18 @@ const App = () => {
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
           <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold">For Developers</h2>
+            <h2 className="text-2xl font-bold">Explore</h2>
             <p className="mt-2 mb-4">
-              Browse our React jobs and start your career today
+              Add trip details such as location, date, itinerary, etc.
             </p>
             <a
               href="/jobs.html"
               className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
             >
-              Browse Jobs
+              Add Trip
             </a>
           </div>
-          <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
+          {/* <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
             <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
@@ -89,7 +52,7 @@ const App = () => {
             >
               Add Job
             </a>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
@@ -98,35 +61,35 @@ const App = () => {
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          Current Trips
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {/* <!-- Job Listing 1 --> */}
           <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
               <div className="mb-6">
-                <div className="text-gray-600 my-2">Full-Time</div>
-                <h3 className="text-xl font-bold">Senior React Developer</h3>
+                <div className="text-gray-600 my-2">November 3rd, 2025</div>
+                <h3 className="text-xl font-bold">Paris, France</h3>
               </div>
 
               <div className="mb-5">
-               We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
+               Itinerary Details
               </div>
 
-              <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3>
+              {/* <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3> */}
 
               <div className="border border-gray-100 mb-5"></div>
 
               <div className="flex flex-col lg:flex-row justify-between mb-4">
-                <div className="text-orange-700 mb-3">
+                {/* <div className="text-orange-700 mb-3">
                   <i className="fa-solid fa-location-dot text-lg"></i>
                   Boston, MA
-                </div>
+                </div> */}
                 <a
                   href="job.html"
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
-                 Read More
+                 View Trip
                 </a>
               </div>
             </div>
@@ -135,28 +98,28 @@ const App = () => {
           <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
               <div className="mb-6">
-                <div className="text-gray-600 my-2">Remote</div>
-                <h3 className="text-xl font-bold">Front-End Engineer (React)</h3>
+                <div className="text-gray-600 my-2">November 25th</div>
+                <h3 className="text-xl font-bold">London, England</h3>
               </div>
 
               <div className="mb-5">
-               Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion...
+               Itinerary Details
               </div>
 
-              <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
+              {/* <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3> */}
 
               <div className="border border-gray-100 mb-5"></div>
 
               <div className="flex flex-col lg:flex-row justify-between mb-4">
-                <div className="text-orange-700 mb-3">
+                {/* <div className="text-orange-700 mb-3">
                   <i className="fa-solid fa-location-dot text-lg"></i>
-                  Miami, FL
-                </div>
+                  Boston, MA
+                </div> */}
                 <a
                   href="job.html"
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
-                 Read More
+                 View Trip
                 </a>
               </div>
             </div>
@@ -165,28 +128,28 @@ const App = () => {
           <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
               <div className="mb-6">
-                <div className="text-gray-600 my-2">Remote</div>
-                <h3 className="text-xl font-bold">React.js Developer</h3>
+                <div className="text-gray-600 my-2">December 10th, 2025</div>
+                <h3 className="text-xl font-bold">Venice, Italy</h3>
               </div>
 
               <div className="mb-5">
-                Are you passionate about front-end development? Join our team in vibrant Brooklyn, NY, and work on exciting projects that make a difference...
+               Itinerary Details
               </div>
 
-              <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
+              {/* <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3> */}
 
               <div className="border border-gray-100 mb-5"></div>
 
               <div className="flex flex-col lg:flex-row justify-between mb-4">
-                <div className="text-orange-700 mb-3">
+                {/* <div className="text-orange-700 mb-3">
                   <i className="fa-solid fa-location-dot text-lg"></i>
-                  Brooklyn, NY
-                </div>
+                  Boston, MA
+                </div> */}
                 <a
                   href="job.html"
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
-                 Read More
+                 View Trip
                 </a>
               </div>
             </div>
@@ -199,7 +162,7 @@ const App = () => {
       <a
         href="jobs.html"
         className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
+        >View All Trips</a
       >
     </section>
     </>
