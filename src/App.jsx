faf30e0d3b1c0b172f4cca4ca99ee17da097b607
Jahi Omari Ghansam <85849036+jahi-omari@users.noreply.github.com>
2025-11-05 20:38:01 -0500

    Added Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
 import React from 'react'
 import Navbar from './components/navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <>
     {/* <!-- Navbar --> */}
-      <Navbar/>
+    <Navbar/>
 
     {/* <!-- Hero --> */}
-    <section className="bg-indigo-700 py-20 mb-4">
-      <div
-        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-      >
-        <div className="text-center">
-          <h1
-            className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
-          >
-            Plan Your Next Trip
-          </h1>
-          <p className="my-4 text-xl text-white">
-            Keep track of all your past, current and upcoming trips!
-          </p>
-        </div>
-      </div>
-    </section>
+    <Hero/>
 
     {/* <!-- Developers and Employers --> */}
     <section className="py-4">
